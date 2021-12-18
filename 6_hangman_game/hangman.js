69e3d316cb3dd46c37a2474cddbe4a9ce13e17ac
Kyle W <kylew864@gmail.com>
2021-12-18 16:58:17 -0500

    all this nodejs bloat for 1 npm package

--- 6_hangman_game/hangman.js
+++ 6_hangman_game/hangman.js
@@ -1,58 +1,58 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
-const words = require('./words.json').data;
-
-class Hangman {
-  constructor(word, guesses) {
-    this.word = word;
-    this.guesses = guesses;
-    this.guessedLetters = [];
-    this.correctGuesses = [];
-  }
-
-  guessLetter(letter) {
-    this.guessedLetters.push(letter);
-    if (this.word.includes(letter)) {
-      this.correctGuesses.push(letter);
+const fs  = require("file-system");
+const words = fs.readFileSync("misspelled.txt").toString().split("\n");
+const livesStart = 8;
+async function game() {
+  const word = words[Math.floor(Math.random() * words.length)].toUpperCase();
+  let guesses = [];
+  let wrongGuesses = [];
+  let lives = livesStart;
+  let wordDisplay = word.split('').map(() => '_');
+  let gameOver = false;
+  while (!gameOver) {
+    console.log(wordDisplay.join(' '));
+    console.log("Wrong letters: " + wrongGuesses.join(' '));
+    let guess = await prompt(`You have ${lives} lives. Guess a letter: `);
+    console.log('\033[2J');
+    guess = guess.toUpperCase();
+    if (guesses.includes(guess)) {
+      console.log('You already guessed that letter!');
+      continue
+    } else if (word.includes(guess)) {
+      wordDisplay = word.split('').map((letter, i) => {
+        if (letter === guess) {
+          return guess;
+        }
+        return wordDisplay[i];
+      });
+    } else if (guess.length !== 1) {
+      continue
+    } else {
+      console.log('That letter is not in the word!');
+      lives--;
+      wrongGuesses.push(guess);
+    }
+    guesses.push(guess);
+    if (lives === 0) {
+      console.log('You lose! The word was ' + word);
+      gameOver = true;
+    } else if (!wordDisplay.includes('_')) {
+      console.log('You win!');
+      gameOver = true;
     }
   }
-
-  isOver() {
-    return this.guesses === 0 || this.word.split('').every((letter) => this.correctGuesses.includes(letter));
-  }
-
-  isWon() {
-    return this.word.split('').every((letter) => this.correctGuesses.includes(letter));
-  }
-
-  getStatus() {
-    let status = '';
-    this.word.split('').forEach((letter) => {
-      if (this.correctGuesses.includes(letter)) {
-        status += letter;
-      } else {
-        status += '_';
-      }
-    });
-    return status;
-  }
 }
-
 async function main() {
-  const word = words[Math.floor(Math.random() * words.length)];
-  const guesses = await prompt('Enter guesses: ');
-  const game = new Hangman(word, guesses);
-  while (!game.isOver()) {
-    console.log(game.getStatus());
-    const letter = await prompt(`Enter a letter: `);
-    game.guessLetter(letter);
-  }
-  if (game.isWon()) {
-    console.log('You won!');
-  } else {
-    console.log('You lost!');
+  let playAgain = true;
+  console.log("Welcome to Mispelled Hangman! This is normal Hangman, but every word is misspelled!");
+  while (playAgain) {
+    await game();
+    playAgain = await prompt('Play again? (y/n) ');
+    playAgain = playAgain?.[0]?.toLowerCase() === 'y';
   }
   rl.close();
+  console.log('Bye!');
 }
-main()
+main();
--- 6_hangman_game/readme.md
+++ 6_hangman_game/readme.md
@@ -1,3 +1,3 @@
 ### Q: Why is this so lame?
 
-A: I wrote less than 10% of the code. Github Copilot filled in the rest. I'm not going to put in a ton of effort for a game that's so simple.
+A: I'm not going to put in a ton of effort for a game that's so simple.
